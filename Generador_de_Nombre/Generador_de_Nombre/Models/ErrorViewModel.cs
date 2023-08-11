f1e38271637991ee7cd2fa4cef1c7e6254d9a258
LuisRomero05 <101825816+LuisRomero05@users.noreply.github.com>
2023-08-11 00:11:12 -0600

    Generador_de_Nombre

---
+++ Generador_de_Nombre/Generador_de_Nombre/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Generador_de_Nombre.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
